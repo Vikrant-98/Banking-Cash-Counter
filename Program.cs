1f26e0f75ef2240c3f3b80c63950703976641b9f
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-02 21:22:29 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banking_Cash_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Banking Cash Counter");
+        }
+    }
+}
