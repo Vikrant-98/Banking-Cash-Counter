b26bee50aa8f5fad9bfb96fbdacb87d9fba2b089
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-02 22:28:39 +0530

    Balance to corresponding Account

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace Banking_Cash_Counter
     class Program
     {
         public int accountNumber;
+        int index = 0;
+        int[] balance = new int[10] ;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Banking Cash Counter");
@@ -21,10 +23,12 @@ namespace Banking_Cash_Counter
                 accountNumber = rand.Next();
                 accountNumber = (accountNumber % 90000) + 10000 ;               //5 Digit Random Account Number  
                 myQueue.Enqueue(accountNumber);
+                balance[i] = rand.Next();
+                balance[i] = balance[i] % 10000;
             }
-            foreach (var element in myQueue)
+            foreach (var element in myQueue)                                    //Account Number and Balance
             {
-                Console.WriteLine(element);
+                Console.WriteLine("Account Number "+ element + " Balance is : " +balance[index++]);
             }
         }
     }
